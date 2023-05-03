a52ab8b6d49a3e425ff90dbc8973c1f3a23ca762
merrittj2012 <35246157+merrittj2012@users.noreply.github.com>
2023-05-03 14:25:48 -0600

    Update main.py

--- Python/Queueing/main.py
+++ Python/Queueing/main.py
@@ -4,7 +4,7 @@ from PlaneNode import PlaneNode
 if __name__ == "__main__":
     plane_queue = PlaneQueue()
     
-    # TODO: Read in arriving flight codes and whether a flight has landed. 
+    #       Read in arriving flight codes and whether a flight has landed. 
     #       Print the queue after every push() or pop() operation. If the user
     #       entered "landed", print which flight has landed. Continue until -1 
     #       is read.
