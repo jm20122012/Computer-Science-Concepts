ccf99b6e889d214ef8d19f77ea9310c1446a01c4
merrittj2012 <35246157+merrittj2012@users.noreply.github.com>
2023-05-03 12:34:16 -0600

    Update LinkedList.py

--- Python/Linked-Lists/LinkedList.py
+++ Python/Linked-Lists/LinkedList.py
@@ -1,8 +1,11 @@
+# This class shows how to implement a custom linked list, also containing methods to insert data into the linked list
+# in ascending order.
 class LinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
 
+        
     def append(self, new_node):
         if self.head == None:
             self.head = new_node
@@ -11,6 +14,7 @@ class LinkedList:
             self.tail.next = new_node
             self.tail = new_node
 
+            
     def prepend(self, new_node):
         if self.head == None:
             self.head = new_node
@@ -19,6 +23,7 @@ class LinkedList:
             new_node.next = self.head
             self.head = new_node
 
+            
     def insert_after(self, current_node, new_node):
         if self.head == None:
             self.head = new_node
@@ -30,6 +35,7 @@ class LinkedList:
             new_node.next = current_node.next   # Point the new node.next at 
             current_node.next = new_node
             
+            
     # TODO: Write insert_in_ascending_order() method
     def insert_in_ascending_order(self, new_node):
         # Handle case where no head and no tail, meaning first insert
@@ -38,17 +44,20 @@ class LinkedList:
             self.tail = new_node
             return
             
+            
         # First check for case if new node data is less than head data, in which case prepend
         if new_node.data < self.head.data:
             #print(f"Prepending list with new node data <{new_node.data}>")
             self.prepend(new_node)
             return 
         
+        
         # Check to see if new node data is greater than tail data, in which case append
         elif new_node.data > self.tail.data:
             #print(f"Appending list with new node data <{new_node.data}")
             self.append(new_node)
             return
+            
             
         # Next go through the list and see if the new node data is less than the next nodes data
         # If it is, insert the new node after the current node
@@ -65,8 +74,6 @@ class LinkedList:
                 current_node = current_node.next
                 
 
-                    
-   
     def remove_after(self, current_node):
         # Special case, remove head
         if (current_node == None) and (self.head != None):
@@ -80,6 +87,7 @@ class LinkedList:
             if succeeding_node == None: # Remove tail
                 self.tail = current_node
 
+                
     def print_list(self):
         cur_node = self.head
         while cur_node != None: # When cur node reaches the tail, tail.next should be None, causing loop to stop
